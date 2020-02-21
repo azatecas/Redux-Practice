d4f91a241450b758971fc0c3a2a34987f812176c
Luis PM <luisp237@gmail.com>
2020-02-20 19:36:43 -0500

    set up provider, store, made reducers directory

---
+++ project/src/reducers/index.js
@@ -0,0 +1,20 @@
+//setup initialState
+const initialState = {
+    players: [],
+    isFetching: false,
+    error: ""
+}
+
+//setup an empty reducer
+const playerReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
+
+export default playerReducer;
